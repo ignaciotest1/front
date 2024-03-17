190701d3b35d76697890a572b1baa5aae9ffa247
Diego Oviedo <diego.oviedo@eldars.com.ar>
2024-03-17 16:47:44 -0300

    fix: cmabios en dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -14,25 +14,45 @@ const Dashboard = () => {
   }, [dispatch]);
 
   return (
-    <section className="flex w-full min-h-screen text-black flex-col items-center justify-between py-24 px-10 lg:px-0">
-      <div className="rounded-[20px] w-full h-screen bg-white p-6">
-        {users ? (
-          users.map((user: User) => (
-            <UserDashboard
-              key={user.userId}
-              image={user.image}
-              status={user.status}
-              name={user.name}
-              email={user.email}
-              role={user.role}
-            />
-          ))
-        ) : (
-          <p>Loading</p>
-        )}
-      </div>
+    <section className="overflow-x-auto bg-white p-5 rounded-3xl">
+      <table className="table-auto min-w-full">
+        <thead>
+          <tr>
+            <th className="px-4 py-2">Perfil</th>
+            <th className="px-4 py-2">Nombre</th>
+            <th className="px-4 py-2">Email</th>
+            <th className="px-4 py-2">Estado</th>
+            <th className="px-4 py-2">Rol</th>
+            <th className="px-4 py-2">Aceptar / Rechazar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <UserDashboard key={user._id!} user={user} />
+          ))}
+        </tbody>
+      </table>
     </section>
   );
 };
 
 export default Dashboard;
+
+// <section className="flex w-full min-h-screen text-black flex-col items-center justify-between py-24 px-10 lg:px-0">
+//   <div className="rounded-[20px] w-full h-screen bg-white p-6">
+//     {users ? (
+//       users.map((user: User) => (
+//         <UserDashboard
+//           key={user.userId}
+//           image={user.image}
+//           status={user.status}
+//           name={user.name}
+//           email={user.email}
+//           role={user.role}
+//         />
+//       ))
+//     ) : (
+//       <p>Loading</p>
+//     )}
+//   </div>
+// </section>
