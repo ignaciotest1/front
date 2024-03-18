1fbca75a39173074d6e45cc11773a9b38ea1bc98
Juan Segundo <juansepincha@gmail.com>
2024-03-17 23:25:24 -0300

    route protect module

--- src/app/modulos/page.tsx
+++ src/app/modulos/page.tsx
@@ -9,18 +9,20 @@ const Modulos = () => {
   // const { data: session } = useSession();
 
   return (
-    <section className="w-full  h-full min-h-[500px] flex flex-col justify-start items-center text-white gap-5">
-      {/* <h1> Bienvenido {session?.user ? session.user.name : "Usuario"}</h1> */}
-      <div className="grid grid-cols-2 col-span-1 gap-3">
-        {modulos.map((modulo, i) => (
-          <Link href={`/modulos/${modulo.link}`} key={i}>
-            <div className="w-[200px] h-full rounded-lg text-black bg-white hover:bg-slate-100 flex items-center justify-center p-4 sombra transform hover:scale-105 transition duration-500 ease-in-out">
-              <p>{modulo.name}</p>
-            </div>
-          </Link>
-        ))}
-      </div>
-    </section>
+    <ProtectedRoute requiresAuth={true}>
+      <section className="w-full  h-full min-h-[500px] flex flex-col justify-start items-center text-white gap-5">
+        {/* <h1> Bienvenido {session?.user ? session.user.name : "Usuario"}</h1> */}
+        <div className="grid grid-cols-2 col-span-1 gap-3">
+          {modulos.map((modulo, i) => (
+            <Link href={`/modulos/${modulo.link}`} key={i}>
+              <div className="w-[200px] h-full rounded-lg text-black bg-white hover:bg-slate-100 flex items-center justify-center p-4 sombra transform hover:scale-105 transition duration-500 ease-in-out">
+                <p>{modulo.name}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </section>
+    </ProtectedRoute>
   );
 };
 
