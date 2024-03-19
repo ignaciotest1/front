7284368fc5eabc6385f8a3abd84b44d759c29299
Juan Segundo <juansepincha@gmail.com>
2024-03-18 22:31:41 -0300

    protect route

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { selectUsers, fetchUsersAsync } from "../../redux/slices/userSlice";
 import { User } from "@/types";
 import UserDashboard from "@/components/UserDashboard";
+import ProtectedRoute from "../routeprotect";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
@@ -14,25 +15,27 @@ const Dashboard = () => {
   }, [dispatch]);
 
   return (
-    <section className="overflow-x-auto bg-white p-5 rounded-3xl">
-      <table className="table-auto min-w-full">
-        <thead>
-          <tr>
-            <th className="px-4 py-2">Perfil</th>
-            <th className="px-4 py-2">Nombre</th>
-            <th className="px-4 py-2">Email</th>
-            <th className="px-4 py-2">Estado</th>
-            <th className="px-4 py-2">Rol</th>
-            <th className="px-4 py-2">Aceptar / Rechazar</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <UserDashboard key={user._id!} user={user} />
-          ))}
-        </tbody>
-      </table>
-    </section>
+    <ProtectedRoute requiresAuth={true}>
+      <section className="overflow-x-auto bg-white p-5 rounded-3xl">
+        <table className="table-auto min-w-full">
+          <thead>
+            <tr>
+              <th className="px-4 py-2">Perfil</th>
+              <th className="px-4 py-2">Nombre</th>
+              <th className="px-4 py-2">Email</th>
+              <th className="px-4 py-2">Estado</th>
+              <th className="px-4 py-2">Rol</th>
+              <th className="px-4 py-2">Aceptar / Rechazar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <UserDashboard key={user._id!} user={user} />
+            ))}
+          </tbody>
+        </table>
+      </section>
+    </ProtectedRoute>
   );
 };
 
